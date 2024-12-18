63f8e6e4605c1e65189b5165338c90b2f48d6cfa
CLARA WÄPPLING <clara.wappling@gmail.com>
2024-12-18 12:43:50 +0100

    ECSS-17 cleaned up some code

--- JS/bestsellers.js
+++ JS/bestsellers.js
@@ -1,3 +1,5 @@
+
+//FETCHING THE BEST-SELLERS AND ADDING THEM TO THE DOM
 async function fetchBestSellers() {
     try {
         const response = await fetch('https://ecommerce-api-sepia.vercel.app/products/best-sellers');
@@ -9,39 +11,33 @@ async function fetchBestSellers() {
 
         const bestSellersContainer = document.querySelector(".best-sellers-container");
 
-        // Loop through the products and render each one
+        // Go through the products and render each one
         products.forEach((product) => {
             const productCard = document.createElement("div");
             productCard.classList.add("product-card");
 
-            // Product Image
             const imgElement = document.createElement("img");
             imgElement.src = product.image;
             imgElement.alt = product.name;
             imgElement.classList.add("product-image");
 
-            // Product Name
             const nameElement = document.createElement("h3");
             nameElement.innerHTML = product.name;
             nameElement.classList.add("product-name");
 
-            // Product Description
             const descriptionElement = document.createElement("p");
             descriptionElement.innerHTML = product.description;
             descriptionElement.classList.add("product-description")
-
-            // Product Price
+        
             const priceElement = document.createElement("p");
-        priceElement.innerHTML = `${product.price.$numberDecimal} SEK`;
+            priceElement.innerHTML = `${product.price.$numberDecimal} SEK`;
             priceElement.classList.add("product-price");
-
-            // Append all elements to the product card
+    
             productCard.appendChild(imgElement);
             productCard.appendChild(nameElement);
             productCard.appendChild(descriptionElement);
             productCard.appendChild(priceElement);
             
-            // Append product card to the container
             bestSellersContainer.appendChild(productCard);
         });
     } catch (error) {
